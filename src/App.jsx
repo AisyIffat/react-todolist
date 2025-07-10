7c0d05d938dca5043edf2b9daa9e3e8c5c22c574
Aisy Iffat <aisy23iffat@gmail.com>
2025-07-10 14:20:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import TodoList from "./assets/components/todolist";
+import AddTodoForm from "./assets/components/addtodoform";
+
+function App() {
+  return (
+    <div className="card rounded shadow-sm" style={{maxWidth: "500px",margin: "60px auto"}}>
+      <div className="card-body">
+        <h3 className="card-title mb-3">My Todo List</h3>
+        <TodoList />
+        <div className="mt-4">
+          <AddTodoForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
